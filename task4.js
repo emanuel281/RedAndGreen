6fb2ea22fb28b9221275900a38c8b93fb2b2ed58
Emanuel <emanuel@projectcodex.co>
2015-02-18 16:41:19 +0200

    Finished Task 4

---
+++ task4.js
@@ -0,0 +1,13 @@
+document.getElementsByTagName('div')[0].style.backgroundColor = "green";
+
+document.getElementsByTagName('button')[0].addEventListener("click", function(){
+	if(document.getElementsByTagName('div')[0].style.backgroundColor === "green"){
+		document.getElementsByTagName('div')[0].style.backgroundColor = "red";
+		document.getElementsByTagName('button')[0].innerHTML = "Make green";
+	}
+	else
+	{
+		document.getElementsByTagName('div')[0].style.backgroundColor = "green";
+		document.getElementsByTagName('button')[0].innerHTML = "Make red";
+	}
+});
