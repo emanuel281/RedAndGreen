040494e13e15e307911d44af6f91442bd79a8537
emauel281 <emanuel@projectcodex.co>
2015-02-25 08:52:25 +0200

    Modified task4.js

--- task4.js
+++ task4.js
@@ -1,13 +1,16 @@
-document.getElementsByTagName('div')[0].style.backgroundColor = "green";
-
 document.getElementsByTagName('button')[0].addEventListener("click", function(){
-	if(document.getElementsByTagName('div')[0].style.backgroundColor === "green"){
-		document.getElementsByTagName('div')[0].style.backgroundColor = "red";
-		document.getElementsByTagName('button')[0].innerHTML = "Make green";
+
+	if(document.getElementsByTagName('div')[0].style.backgroundColor === "red"){
+
+		document.getElementsByTagName('div')[0].style.backgroundColor = "green";
+		document.getElementsByTagName('button')[0].innerHTML = "Make red";
+
 	}
 	else
 	{
-		document.getElementsByTagName('div')[0].style.backgroundColor = "green";
-		document.getElementsByTagName('button')[0].innerHTML = "Make red";
+
+		document.getElementsByTagName('div')[0].style.backgroundColor = "red";
+		document.getElementsByTagName('button')[0].innerHTML = "Make green";
+
 	}
 });
